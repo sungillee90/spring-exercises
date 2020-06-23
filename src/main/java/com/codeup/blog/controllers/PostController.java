ed69b8e2efea489ddbda661c0f5cf90ff2cfff4e
SUNG IL LEE <sungil.lee90@gmail.com>
2020-06-23 15:51:37 -0500

    feat: added delete, edit functionality.

--- src/main/java/com/codeup/blog/controllers/PostController.java
+++ src/main/java/com/codeup/blog/controllers/PostController.java
@@ -1,27 +1,34 @@
 package com.codeup.blog.controllers;
 
+import com.codeup.blog.daos.PostsRepository;
 import com.codeup.blog.models.Post;
 import org.springframework.stereotype.Controller;
 
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class PostController {
 
+    // Dependency injection
+    private PostsRepository postsDao;
+    // Dependency injection
+    public PostController(PostsRepository postsRepository) {
+        postsDao = postsRepository;
+    }
+
     @GetMapping("/posts")
 //    @ResponseBody
     public String doPost(Model model) {
-        ArrayList<Post> postsList = new ArrayList<>();
-        postsList.add(new Post("First","first blog post"));
-        postsList.add(new Post("Second","second blog post"));
-        postsList.add(new Post("Third","third blog post"));
-        postsList.add(new Post("Fourth","fourth blog post"));
+        List<Post> postsList = postsDao.findAll();
+//        postsList.add(new Post("First","first blog post"));
+//        postsList.add(new Post("Second","second blog post"));
+//        postsList.add(new Post("Third","third blog post"));
+//        postsList.add(new Post("Fourth","fourth blog post"));
+
         model.addAttribute("noPostsFound", postsList.size() == 0);
         model.addAttribute("posts", postsList);
         return "posts/index";
@@ -43,7 +50,31 @@ public class PostController {
     @PostMapping("/posts/create")
     @ResponseBody
     public String create() {
+        Post newPost = new Post("dependency Injection Test","Testing");
+        postsDao.save(newPost);
         return "create a new post";
     }
+
+//    @PutMapping("/posts/{id}/edit")
+//    @ResponseBody
+//    public String update(@PathVariable long id) {
+//        // find a post
+//        Post foundPost = postsDao.getOne(id); // SELECT * FROM posts WHERE id = ?
+//        // edit the post
+//        foundPost.setTitle("This is cool title Testing");
+//        // save the post
+//        postsDao.save(foundPost);
+//        return "post updated";
+//    }
+
+
+//    @DeleteMapping("/posts/{id}/delete")
+//    @ResponseBody
+//    public String destroy(@PathVariable long id) {
+//        postsDao.deleteById(id);
+//        return "post Successfully deleted";
+//    }
+
+
 
 }
